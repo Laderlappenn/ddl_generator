52e9d467c63add8b9003b61088ceccb8c316d5e7
unknown <@MB-38CA84D27A10.NITEC.KZ>
2024-05-03 15:55:22 +0500

    upd

--- utils.py
+++ utils.py
@@ -9,13 +9,13 @@ def get_encoding_type(file):
 
 def get_ddl_type(dtype):
     if dtype == 'int64':
-        return ("Int64")
+        return "Int64"
     elif dtype == 'float64':
-        return ("Float64")
+        return "Float64"
     elif dtype == 'datetime64[ns]':
-        return ("DateTime")
+        return "DateTime"
     elif dtype == 'object':
-        return ("String")
+        return "String"
 
 def get_engine_type(df):
     if df.shape[0] >= 1_000_000:
