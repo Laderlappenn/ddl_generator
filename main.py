c0b2bbeeb5caa4c68a03aae17951275f77028b7a
unknown <@MB-38CA84D27A10.NITEC.KZ>
2024-05-03 15:42:35 +0500

    upd

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from tkinter import filedialog, messagebox
 from tkinter.messagebox import *
 import pandas as pd
 
-import ctypes, threading
+import ctypes
+import threading
 
 
 from utils import get_encoding_type, get_ddl_type, get_engine_type
@@ -59,7 +60,7 @@ class DDLGeneratorApp:
             if self.filepath:
                 if self.filepath.endswith('.csv'):
                     try:
-                        df = pd.read_csv(self.filepath)
+                        df = pd.read_csv(self.filepath, low_memory=False)
                     except UnicodeDecodeError as unicode_error:
                         popup = ctypes.windll.user32.MessageBoxW
                         threading.Thread(target=lambda: popup(None, 'Автоматическое распознование кодировки', f'{unicode_error.__class__.__name__}', 0)).start()
@@ -76,19 +77,19 @@ class DDLGeneratorApp:
 
                 ddl_statements = []
                 ddl_statements.append(f"CREATE TABLE {self.database_name_text.get()}.{self.table_name_text.get()}\n(")
-                ddl_statements.append(f"    `{primary_key}` {primary_key_type},")
+                ddl_statements.append(f"    `{primary_key}` {get_ddl_type(primary_key_type)},")
 
                 skip_first_iter = True
                 for column_name, dtype in df.dtypes.items():
                     if skip_first_iter:
                         skip_first_iter = False
                         continue
-                    ddl_statements.append(get_ddl_type(column_name, dtype))
+                    ddl_statements.append(f"    `{column_name}` Nullable({get_ddl_type(dtype)}),")
 
                 ddl_statements.append("    `SDU_LOAD_IN_DT` DateTime DEFAULT now()\n)")
                 ddl_statements.append(f"ENGINE = {get_engine_type(df)}")
                 ddl_statements.append(f"ORDER BY {primary_key}")
-                ddl_statements.append("SETTINGS index_granularity = 8192;")
+                ddl_statements.append("SETTINGS index_granularity = 8192;\n")
 
                 ddl_output = "\n".join(ddl_statements)
                 self.output_text.insert(tk.END, ddl_output)
@@ -96,7 +97,6 @@ class DDLGeneratorApp:
                 messagebox.showerror("Error", "No file selected")
         except Exception as e:
             messagebox.showerror("Error", str(e) + " " + str(e.__class__.__name__))
-
 
 
 def main():
@@ -108,7 +108,8 @@ def main():
 if __name__ == "__main__":
     success, message = update()
     if success:
-        print(message)
+        pass
     else:
         print("Failed:", message)
+
     main()
--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-import os
 from chardet import detect
 
 
@@ -8,15 +7,15 @@ def get_encoding_type(file):
     return detect(rawdata)['encoding']
 
 
-def get_ddl_type(column_name, dtype):
+def get_ddl_type(dtype):
     if dtype == 'int64':
-        return (f"    `{column_name}` Nullable(Int64),")
+        return ("Int64")
     elif dtype == 'float64':
-        return (f"    `{column_name}` Nullable(Float64),")
+        return ("Float64")
     elif dtype == 'datetime64[ns]':
-        return (f"    `{column_name}` Nullable(DateTime),")
+        return ("DateTime")
     elif dtype == 'object':
-        return (f"    `{column_name}` Nullable(String),")
+        return ("String")
 
 def get_engine_type(df):
     if df.shape[0] >= 1_000_000:
